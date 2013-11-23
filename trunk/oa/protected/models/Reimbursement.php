005db1601108c7269c71c25abfe36140eba5a477
Lu Zhang <luzhang@jmlvyou.com>
2013-11-23 10:53:42 +0800

    Add useful functions to model

--- trunk/oa/protected/models/Reimbursement.php
+++ trunk/oa/protected/models/Reimbursement.php
@@ -16,8 +16,52 @@ class Reimbursement extends CActiveRecord{
     }
     public function rules(){
     	return array(
-    		array('username, price', 'safe')
+    		array('username, price, content, approve_time, type', 'safe')
     		);
+    }
+    /**
+     * Add new attributes
+     * @param array $data
+     * @param array $items
+     * @return bool
+     **/
+    public static function add(array $data, array $items){
+        $model = new Reimbursement;
+        $model->attributes = $data;
+        if($model->save()){
+            foreach($items as $item){
+                $itemModel = new ReimbursementItem;
+                $itemModel->attributes = $item;
+                $itemModel->reimbursement_id = $model->id;
+                $itemModel->save();
+            }
+            return true;
+        } 
+        return false;
+    }
+    /**
+     * Update reimbusement record
+     * @param array $data
+     * @param array $items
+     * @return bool
+     **/
+    public static function update(array $data, array $items){
+        if(isset($data['id']))
+            $model = self::model()->findByPk($data['id']);
+        unset($data['id']);
+        $model->attributes = $data;
+        if($model->save()){
+            if(Reimbursement::model()->deleteAllByAttributes(array('reimbursement_id'=>$model->id))){
+                foreach($items as $item){
+                    $itemModel = new ReimbursementItem;
+                    $itemModel->attributes = $item;
+                    $itemModel->reimbursement_id = $model->id;
+                    $itemModel->save();
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
 ?>
