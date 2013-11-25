e355a0344066711d40426524c3da1e67b8286579
Lu Zhang <luzhang@jmlvyou.com>
2013-11-25 08:26:26 +0800

    FinanceController: Update the reimbursement review method

--- trunk/oa/protected/controllers/FinanceController.php
+++ trunk/oa/protected/controllers/FinanceController.php
@@ -4,19 +4,17 @@ class FinanceController extends Controller{
     public function beforeAction(){
         parent::beforeAction();
     }
+    /**
+     * Apply for new reimbursement
+     * @version 1.0 11/25/13 02:40:47
+     **/
 	public function actionReimbursement(){
-		if(isset($_POST['ReimbursementForm'])){
-			$reimbursement = new Reimbursement();
-			$reimbursement->user_id = Yii::app()->user->id;
-			$reimbursement->username = $_POST['ReimbursementForm']['username'];
-			$reimbursement->price = $_POST['ReimbursementForm']['price'];
-			$reimbursement->item = $_POST['ReimbursementForm']['item'];
-			$reimbursement->name = $_POST['ReimbursementForm']['name'];
-			$reimbursement->type = $_POST['ReimbursementForm']['type'];
-            $reimbursement->status = 'pending';
-            $reimbursement->create_time = strtotime('now');
-			$reimbursement->content = serialize($_POST['ReimbursementForm']['content']);
-			if($reimbursement->save()){
+		if(isset($_POST['ReimbursementForm']) && isset($_POST['ReimbursementItemForm'])){
+            $data = $_POST['ReimbursementForm'];
+            $data['user_id'] = Yii::app()->user->id;
+            $itemData = $_POST['ReimbursementItemForm'];
+            $model = Reimbursement::addNew($data, $itemData);
+			if(!$model){
 				$this->redirect(
 					array('notify/success', 
 						'back'=>'finance/reimbursementlist', 
@@ -32,51 +30,62 @@ class FinanceController extends Controller{
 		}
 		
 	}
+    /**
+     * Edit Reimbursement 
+     * @param int $id the id of reimbursement record
+     * @version 1.0
+     **/
     public function actionReimbursementEdit($id){
         if(isset($id)){
-            if(empty($_POST['ReimbursementForm'])){
+            if(empty($_POST['ReimbursementForm']) && empty($_POST['ReimbursementItemForm'])){
                 $result = Reimbursement::model()->findByPk($id);
-                $result->content = unserialize($result->content);
                 $this->render('applyreimbursement', array('result'=>$result));
             }else{
-                $model = new Reimbursement();
-                $model->attributes = $_POST['ReimbursementForm'];
-                $model->content = serialize($_POST['ReimbursementForm']['content']);
+                if(Reimbursement::updateInfo($_POST['ReimbursementForm'], $_POST['ReimbursementItemForm'])){
+                    $this->redirect(
+                        array('notify/success', 
+                            'back'=>'finance/reimbursementlist', 
+                            'title'=>'报销修改成功', 
+                            'content'=>'报销修改成功。'
+                            )
+                        );  
+                }
             }
 
         }
     }
-	public function actionReimbursementlist(){
-        if(Yii::app()->user->role == 'staff'){
-            $result = Reimbursement::model()->findAll('user_id = :userId', array(':userId'=>Yii::app()->user->id));
-        }elseif(Yii::app()->user->role == 'admin' || Yii::app()->user->role == 'superintendent'){
-            $result = Reimbursement::model()->findAll('status != "cancelled"', array());
-        }elseif(Yii::app()->user->role == 'project-manager'){
-            $users = Project::findUserUnderProject(Yii::app()->user->id);
-            $criteria = new CDbCriteria();
-            if(!empty($users)){
-                $criteria->addInCondition('user_id', $users, 'OR');
-            }
-            $criteria->addCondition('user_id = '.Yii::app()->user->id);
-            $result = Reimbursement::model()->findAll($criteria);
-        }
+
+    /**
+     * Show reimbursement List
+     * @param mixed $filters the filters of reimbursement
+     * @version 1.0 11/25/13 02:45:08
+     **/
+	public function actionReimbursementList(array $filters = array()){
+        $result = Reimbursement::getList($filters, Yii::app()->user->role, Yii::app()->user->id);
 		$this->render('reimbursementlist', array('result'=>$result));
 	}
+    /**
+     * Review the reimbursement record
+     * @param int $id reimbursement record id
+     * @return bool
+     * @version 1.0 11/25/13 08:25:41
+     **/
 	public function actionReimbursementReview($id){
 		if(Yii::app()->request->isAjaxRequest && isset($_POST['ReimbursementForm'])){
-            $model = Reimbursement::model()->findByPk($_POST['ReimbursementForm']['id']);
-            $model->status = $_POST['ReimbursementForm']['status'];
-            if($model->save()){
-                echo 'success';
-            }else{
-                echo 'failure';
+            if(in_array($_POST['ReimbursementForm']['status'], array( Reimbursement::STATUS_REJECTED, Reimbursement::STATUS_RESUBMIT, Reimbursement::STATUS_APPROVED))){
+                if(Reimbursement::setStatus($_POST['ReimbursementForm'], $id, Yii::app()->user->id)){
+                    echo 'success';
+                }else{
+                    echo 'failure';
+                }
             }
         }else{
-            $model = Reimbursement::model()->findByPk($id);
-            $model->approve_time = strtotime('now');
-            $model->status = 'approved';
-            if($model->save()){
-                $this->redirect(array('notify/success', 'back'=>'finance/reimbursementlist', 'content'=>'审核成功'));
+            if(in_array($_POST['ReimbursementForm']['status'], array( Reimbursement::STATUS_REJECTED, Reimbursement::STATUS_RESUBMIT, Reimbursement::STATUS_APPROVED))){
+                if(Reimbursement::setStatus($_POST['ReimbursementForm'], $id, Yii::app()->user->id)){
+                    $this->redirect(array('notify/success', 'back'=>'finance/reimbursementlist', 'content'=>'审核成功'));
+                }else{
+                    echo 'failure';
+                }
             }
         }
 	}
