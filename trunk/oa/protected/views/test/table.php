21a90c75b24b5c3f720b5339ddeac09a0c551064
Lu Zhang <luzhang@jmlvyou.com>
2013-11-25 19:10:18 +0800

    The table of users

---
+++ trunk/oa/protected/views/test/table.php
@@ -0,0 +1,12 @@
+<div class="substance">
+    <div class="leftside" style="width:1100px">
+<table class="pretty" cellspacing="0">
+    <tr><th>Task Details</th><th>Progress</th><th>Vital Task</th></tr>
+    <tr><td>Look at Orman's <a href="http://www.premiumpixels.com/simple-little-table-psd/">simple table design</a></td><td>100%</td><td>No</td></tr>
+    <tr><td>Turn table design into a web project</td><td>100%</td><td>Yes</td></tr>
+    <tr><td>Drink another cup of coffee</td><td>50%</td><td>Yes</td></tr>
+    <tr><td>Take a stretch break</td><td>0%</td><td>Yes</td></tr>
+    <tr><td>Post this stuff on the interweb</td><td>100%</td><td>Yes</td></tr>
+    </table>
+    </div>
+    </div>
