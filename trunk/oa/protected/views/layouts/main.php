ac16e5f199d7292f7923c3de3d0d6cea53b51bde
Lu Zhang <luzhang@jmlvyou.com>
2013-11-26 15:51:00 +0800

    some fixture

--- trunk/oa/protected/views/layouts/main.php
+++ trunk/oa/protected/views/layouts/main.php
@@ -10,7 +10,7 @@
 	<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-1.8.0.min.js"></script>
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-ui.js"></script>
     <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-ui-timepicker-addon.js"></script>
-    <link rel="stylesheet" href="h<?php echo Yii::app()->request->baseUrl; ?>/css/jquery-ui.css" />
+    <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/jquery-ui.css" />
     <link rel='stylesheet' type='text/css' href='<?php echo Yii::app()->request->baseUrl; ?>/fullcalendar/fullcalendar.css' />
 	<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/main.js"></script>
     <script type='text/javascript' src='<?php echo Yii::app()->request->baseUrl; ?>/fullcalendar/fullcalendar.js'></script>
